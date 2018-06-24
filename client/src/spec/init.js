ee23375d9d11bd15da974cf08ba6168ff88392ea
Vitali Dettling <vitali.dettling@gmail.com>
2018-06-24 12:05:07 +0200

    karma is now running via launch ignore lock.json launch karma and backend also together karma alone can also be run via npm run included init test

---
+++ client/src/spec/init.js
@@ -0,0 +1,8 @@
+
+describe('test', () => {
+
+    //todo write real test
+    it('this should run when karma starts', () => {
+      expect(true).toBe(true);
+    });
+});
--- client/src/test.ts
+++ client/src/test.ts
@@ -25,8 +25,9 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context = require.context('./spec', true);
 // And load the modules.
 context.keys().map(context);
 // Finally, start Karma to run the tests.
 __karma__.start();
+
